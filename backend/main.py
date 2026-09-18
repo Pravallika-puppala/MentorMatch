9b6a6bb82a70d7013a46f0381d6042d9984cc2d9
Pravallika-puppala <puppalapravallika8@gmail.com>
2026-09-18 18:41:55 +0530

    Allow Vercel frontend in CORS

--- backend/main.py
+++ backend/main.py
@@ -24,10 +24,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://localhost:5500"
-    ],
+   allow_origins=[
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+    "https://mentor-match-pi.vercel.app"
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
